de541402eb4820f65f421b6beb8baaa121763ead
Xikser <Xikser@wp.pl>
2020-11-02 18:26:57 +0100

    Finalize backward slider transform

--- web/assets/js/arrowControl.js
+++ web/assets/js/arrowControl.js
@@ -14,21 +14,20 @@ window.addEventListener('keydown', (e) => {
 
 const changeCurrentSlider = (e, current, next, previous) => {
     const sliderIndex = getSliderIndex(current)
+    //current = slider-0
 
     if(e.key === 'ArrowUp' && sliderIndex < sliderArray.length - 1) {
         current.classList.remove('current-slider')
         next.classList.add('current-slider')
         transformForwardSlider(current, next)
-        changeSliderOpacity(current, next, sliderIndex)
+        changeSliderVisible(e, current, next, sliderIndex)
     }
     
     if(e.key === 'ArrowDown' && sliderIndex > 0 ) {
         current.classList.remove('current-slider')
         previous.classList.add('current-slider')
-        transformBackwardSlider(current, previous, sliderIndex)
-    }
-    
-    if(e.key === 'ArrowDown') {
+        transformBackwardSlider(current, previous)
+        changeSliderVisible(e, current, next, sliderIndex)
     }
 }
 
@@ -64,46 +63,80 @@ const transformForwardSlider = (slider, nextSlider) => {
     nextSlider.classList.remove(sliderClass)
 }
 
-const transformBackwardSlider = (current, previous, index) => {
-    // console.log(slider)
+const transformBackwardSlider = (current, previous) => {
     current.classList.add(sliderClass)
     previous.style.visibility = 'visible'
     previous.classList.remove(hideSlider)
-    clearSliderStyle(previous)
 }
 
-const changeSliderOpacity = (previous, current, index) => {
-    if(index >= 0 && index < 5) {
-        const nextSlider = current.nextElementSibling
-        const thirdSlider = nextSlider.nextElementSibling
+const changeSliderVisible = (e, previous, current, index) => {
+    //current -> previous
+    //next -> current
+
+    if(e.key === 'ArrowUp') {
+        if(index >= 0 && index < 5) {
+            const nextSlider = current.nextElementSibling
+            const thirdSlider = nextSlider.nextElementSibling
+
+            setTimeout(() => {
+                current.style.opacity = '.9'
+                nextSlider.style.opacity = '.3'
+                thirdSlider.style.opacity = '.1'
+            }, 0)
+
+            clearSliderStyle(previous)
+            
+        } else if (index == 5) {
+            const nextSlider = current.nextElementSibling
+
+            setTimeout(() => {
+                current.style.opacity = '.9'
+                nextSlider.style.opacity = '.3'
+                clearSliderStyle(previous)
+            }, 0)
+        }
         
-        previous.style.opacity = '0'
-        current.style.opacity = '.9'
-        nextSlider.style.opacity = '.3'
-        thirdSlider.style.opacity = '.1'
-
-        clearSliderStyle(previous)
-
-    } else if (index == 5) {
-        const nextSlider = current.nextElementSibling
-        previous.style.opacity = '0'
-        current.style.opacity = '.9'
-        nextSlider.style.opacity = '.3'
-        clearSliderStyle(previous)
+        if (index == 6) {
+            current.style.opacity = '.9'
+            clearSliderStyle(previous)
+        }
     }
     
-    if (index == 6) {
-        current.style.opacity = '.9'
-        clearSliderStyle(previous)
+    if(e.key === 'ArrowDown') {
+        if(index == 7) {
+            const nextSlider = previous.previousElementSibling
+            setTimeout(() => {
+                nextSlider.style.opacity = '.9'
+                previous.style.opacity = ''
+            }, 0)
+            
+        } else if (index < 7) {
+            const nextSlider = previous.previousElementSibling
+            const thirdSlider = previous.nextElementSibling
+
+            setTimeout(() => {
+                nextSlider.style.opacity = '.9'
+                previous.style.opacity = '.3'
+                thirdSlider.style.opacity = ''        
+            }, 0)
+        }
+
+        if(index < 6) {
+            const thirdSlider = previous.nextElementSibling
+            const x = thirdSlider.nextElementSibling
+
+            setTimeout(() => {
+                x.style.opacity = ''
+            }, 0)
+        }
     }
 }
 
-const clearSliderStyle = (previous) => {
+const clearSliderStyle = (slider) => {
     const TIME = 1200
 
     setTimeout(() => {
-        previous.style.opacity = ''
-        nextSlider.style.opacity = ''
-        thirdSlider.style.opacity = ''
+        slider.style.opacity = ''
+        slider.style.visibility = ''
     }, TIME)
 }
